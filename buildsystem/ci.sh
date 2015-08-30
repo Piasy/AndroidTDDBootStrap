6338a19ad89a94a85f23891100e350fb9b3b9d47
Piasy <xz4215@gmail.com>
2015-08-30 09:42:38 +0800

    update once (add todo feature)

--- buildsystem/ci.sh
+++ buildsystem/ci.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-buildsystem/testCommon.sh && buildsystem/testCommonAndroid.sh && buildsystem/testModel.sh && buildsystem/testPresentation.sh
+./gradlew :once:once:clean :once:once:testDebug && buildsystem/testCommon.sh && buildsystem/testCommonAndroid.sh && buildsystem/testModel.sh && buildsystem/testPresentation.sh
