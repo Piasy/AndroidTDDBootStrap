f20db02449ed086be4f3586e5ebfc8ae2ac2c78e
Piasy <xz4215@gmail.com>
2016-06-23 22:08:00 +0800

    speed up travis build; not fail lint in travis; update mockito and dexmaker

--- buildsystem/ci.sh
+++ buildsystem/ci.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-buildsystem/checkBase.sh && buildsystem/checkModel.sh && buildsystem/checkApp.sh && buildsystem/checkBuck.sh
+buildsystem/checkBase.sh && buildsystem/checkModel.sh && buildsystem/checkApp.sh #&& buildsystem/checkBuck.sh
